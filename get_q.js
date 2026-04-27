75f26d5c1f5a1d10ad38e7ab3228168b434fa4c1
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-04-27 17:48:04 -0600

    qs

---
+++ get_q.js
@@ -0,0 +1,47 @@
+//mqtt server connection
+
+const client = mqtt.connect("wss://e4f0d50b37b04ea79745872566f605ff.s1.eu.hivemq.cloud:8884/mqtt",{
+    clientId: "web_" + Math.random().toString(16).slice(2, 10),
+    username: "MarcoA",
+    password: "HATeR3__",
+    clean: true
+});
+
+console.log("Connecting to HiveMQ Claud...");
+
+client.on("connect", () => {
+  console.log("Connecting with Outh");
+
+  client.subscribe(["q1","q2","q3"], (err)=>{
+    if(!err){
+        console.log("Subscripcion en q1, q2 y q3 exitosa");
+    }else{
+        console.error("Error en subscripcion q1, q2 y q3:", err);
+    }
+    });
+});
+
+client.on("error", (err) => {
+  console.error("Error:", err);
+});
+
+client.on("message",(topic,message)=>{
+        const value = Number(message.toString());
+        console.log("Topic: ",topic,"Value: ",value);
+
+        if(topic === "q1"){
+            g1.value = value;
+            q1 = g1.value * Math.PI/180;
+        }
+
+        if(topic === "q2"){
+            g2.value = value;
+            q2 = g2.value * Math.PI/180;
+        }
+
+        if(topic === "q3"){
+            g3.value = value;
+            q3 = g3.value * Math.PI/180;
+        }
+
+    });
